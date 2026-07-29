64f1ef75ca19b6ca0a9f4f169e4040ca6cb7dc79
Aoi-nao <aoisuzume4869@gmail.com>
2026-07-29 15:54:11 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,32 @@
+/*======================================
+
+CHUN & DAM
+Wedding Website
+
+Configuration File
+
+======================================*/
+
+const WEDDING = {
+
+    bride: "CHUN",
+
+    groom: "DAM",
+
+    weddingDate: "17 • 10 • 2026",
+
+    invitationTitle: "WEDDING INVITATION",
+
+    buttonText: "MỞ THIỆP",
+
+    quote: "Together is a beautiful place to be",
+
+    music: "music/wedding.mp3",
+
+    heroImage: "",
+
+    brideImage: "images/bride.jpg",
+
+    groomImage: "images/groom.jpg"
+
+};
